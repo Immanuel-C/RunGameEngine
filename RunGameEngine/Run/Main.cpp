82b94e7eec1d0e9b1a4e41a5b2de711f06e656f8
Immanuel Charles <73500956+immanuelcharlesiscool@users.noreply.github.com>
2022-01-10 15:29:52 -0800

    changed main

--- RunGameEngine/Run/Main.cpp
+++ RunGameEngine/Run/Main.cpp
@@ -1,7 +1,6 @@
 // Run
 #include "RunInclude.h"
 
-
 int main(void)
 {
     /* Create a windowed mode window and its OpenGL context */
@@ -15,7 +14,7 @@ int main(void)
 
     SoundManager soundManager;
 
-    Shape triangles[11];
+    Shape triangles[2];
     float sizeOfTriangles = sizeof(triangles) / sizeof(triangles[0]);
     
     {
@@ -25,179 +24,28 @@ int main(void)
              800.0f, 100.0f,  1.0f, 0.0f,   // bottom right
              750.0f, 500.0f,  0.5f, 1.0f,   // top 
              600.0f, 100.0f,  0.0f, 0.0f,   // bottom left
-             600.0f, 250.0f,  0.0f, 0.0f,   // bottom left
         };
 
         std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 3,   // first triangle
+            0, 1, 2,   // first triangle
         };
 
         triangles[0] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/Lake.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             500.0f, 100.0f,  1.0f, 0.0f,   // bottom right
-             550.0f, 500.0f,  0.5f, 1.0f,   // top 
-             400.0f, 100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[1] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/WaterFall.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             1000.0f, 100.0f,  1.0f, 0.0f,   // bottom right
-             750.0f, 500.0f,  0.5f, 1.0f,   // top 
-             900.0f, 100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[2] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/container.jpg"));
     }    
     {
         std::vector<float> NDCvertices =
         {
             // positions    // texture coords
-             -1000.0f, 100.0f,  1.0f, 0.0f,   // bottom right
-             -750.0f, -500.0f,  0.5f, 1.0f,   // top 
-             900.0f, 100.0f,  0.0f, 0.0f,   // bottom left
+             1000.0f,-100.0f,  1.0f, 0.0f,   // bottom right
+             850.0f, -500.0f,  0.5f, 1.0f,   // top 
+             700.0f, -100.0f,  0.0f, 0.0f,   // bottom left
         };
 
         std::vector<unsigned int> NDCindices = {  // note that we start from 0!
             0, 1, 2,   // first triangle
         };
 
-        triangles[3] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/wall.jpg"));
-    }    
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             750.0f, -500.0f,  0.5f, 1.0f,   // top 
-             900.0f, -100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[4] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/Forest.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             -1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             750.0f, 500.0f,  0.5f, 1.0f,   // top 
-             -900.0f, -100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[5] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/wall.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             -750.0f, -500.0f,  0.5f, 1.0f,   // top 
-             900.0f, -100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[6] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/WaterFall.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             -1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             -750.0f, -500.0f,  0.5f, 1.0f,   // top 
-             900.0f, -100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[7] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/container.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             -1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             750.0f, -500.0f,  0.5f, 1.0f,   // top 
-             900.0f, 100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[8] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/Lake.jpg"));
-    }
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             -1000.0f, 100.0f,  1.0f, 0.0f,   // bottom right
-             -750.0f, -500.0f,  0.5f, 1.0f,   // top 
-             900.0f,  100.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[9] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/Forest.jpg"));
-    }  
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             -1050.0f, 500.0f,  0.5f, 1.0f,   // top 
-             -500.0f, -10.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[10] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/wall.jpg"));
-    }    
-    {
-        std::vector<float> NDCvertices =
-        {
-            // positions    // texture coords
-             -1000.0f, -100.0f,  1.0f, 0.0f,   // bottom right
-             -1050.0f, 500.0f,  0.5f, 1.0f,   // top 
-             -500.0f, 10.0f,  0.0f, 0.0f,   // bottom left
-        };
-
-        std::vector<unsigned int> NDCindices = {  // note that we start from 0!
-            0, 1, 2,   // first triangle
-        };
-
-        triangles[10] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/wall.jpg"));
+        triangles[1] = renderer.createShape(NDCvertices, NDCindices, LoadFile::loadShader("Res/Shader/VertShader.glsl", "Res/Shader/FragShader.glsl"), LoadFile::loadTexture("Res/Textures/Lake.jpg"));
     }
 
     int isWireframe = GL_FILL;
@@ -205,16 +53,27 @@ int main(void)
     soundManager.play("Res/Audio/explosion.wav");
 
     /* Loop until the user closes the window */
+
+    float colours[3];
+    int lengthOfColours = sizeof(colours) / sizeof(colours[0]);
+
+    srand(time(NULL));
+    for (int i = 0; i < lengthOfColours; i++)
+    {
+        colours[i] = (float)(rand() % 256) / 256.0f;
+    }
+
+
     while (!window.shouldClose())
     {
-        window.setWindowColor(0.25f, 0.123f, 0.65f, 1.0f);
+        window.setWindowColor(colours[0], colours[1], colours[2], 1.0f);
         
-        if (Input::isKeyPressed(Keys::F1))
+        if (Input::isMouseButtonPressed(MouseButtons::SideButton1))
         {
             isWireframe = GL_LINE;
         }
 
-        if (Input::isKeyPressed(Keys::F2))
+        if (Input::isMouseButtonPressed(MouseButtons::SideButton2))
         {
             isWireframe = GL_FILL;
         }
@@ -225,7 +84,7 @@ int main(void)
         for (int i = 0; i < sizeOfTriangles; i++)
         {
             triangles[i].setCamera(camera);
-            triangles[i].setRotation(120 * i, glm::vec3(0.0f, 0.0f, 1.0f));
+            triangles[i].setRotation(glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
             renderer.draw(triangles[i]);
         }
 
